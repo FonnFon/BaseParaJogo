6c3e9bcdfb96b831d315446692d0efd405a161b4
FonnFon <143230785+FonnFon@users.noreply.github.com>
2025-01-21 19:33:16 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,16 +1,32 @@
+var wizard, wizardWalk;
+var fundoImg;
 
 
+function preload() {
+
+    fundoImg = loadImage("fundo.jpg");
+    /*wizardWalk = loadAnimation("wizard_sprite_1.png",
+    "wizard_sprite_2.png")*/
+}
 
 function setup() {
-  
+    createCanvas(1600, 682);
+    /*wizard = createSprite(50, 50);
+    wizard.addAnimation("walking", wizardWalk)  
+    wizard.velocityX = 5;
+    wizard.velocityY = 5*/
 }
 
 function draw() {
+    background(fundoImg);
 
-  
-  drawSprites();
+
+    /*if(keyDown("d")) {
+        wizard.velocityX = velocityX + 5
+    }
+
+    if(keyDown("a")) {
+        wizard.velocityY = velocityX - 5
+    }*/
+    drawSprites();
 }
-
-
-
-
